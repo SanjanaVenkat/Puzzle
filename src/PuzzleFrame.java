35af48fa301459f851103f13f4a222ab07df302e
Sanjana Venkat <349973@beaverton.k12.or.us>
2018-05-25 09:47:56 -0700

    added split images

--- src/PuzzleFrame.java
+++ src/PuzzleFrame.java
@@ -8,9 +8,14 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class PuzzleFrame extends JPanel {
+	int rows;
+	int columns;
 	BufferedImage original;
+	BufferedImage[][] newImages;
 	int widthImage;
 	int heightImage;
+	boolean drawImage;
+	
 	
 	public PuzzleFrame() {
 		super();
@@ -24,10 +29,22 @@ public class PuzzleFrame extends JPanel {
 		super.paintComponent(g);
 		g.setColor(Color.PINK);
 		g.fillRect(0, 0, getWidth(), getHeight());
+		if (drawImage == false) {
 		drawOriginal(g);
+		}
+		else if (drawImage == true) {
+		 drawnewImages(g);
+		}
 	}
 	public void drawOriginal(Graphics g) {
 		g.drawImage(original, 0, 0, null);
+	}
+	public void drawnewImages(Graphics g) {
+		for (int i = 0; i < newImages.length; i++) {
+			for (int j = 0; j < newImages.length; j++) {
+				g.drawImage(newImages[i][j], (original.getWidth() / columns) * i, (original.getHeight() / rows) * j, null );
+			}
+		}
 	}
 	public void changeImage(String filePath) {
 		try {
@@ -46,5 +63,16 @@ public class PuzzleFrame extends JPanel {
 	//getter for image width
 	public int getImageWidth() {
 		return widthImage;
+	}
+	
+	public void splitImage() {
+		newImages = new BufferedImage[rows][columns];
+		for (int i = 0; i < newImages.length; i++) {
+			for (int j = 0; j < newImages.length; j++) {
+				newImages[i][j] = original.getSubimage((original.getWidth() / columns) * i, (original.getHeight() / rows) * j,
+						original.getWidth() / columns, original.getHeight() / rows);
+			}
+		}
+	drawImage = true;
 	}
 }
